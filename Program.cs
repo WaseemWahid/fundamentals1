b530e6e8f337f4a7ed076ee27dc5fe8846eee96c
Waseem Wahid <wahid.waseem@gmail.com>
2022-02-06 12:12:48 -0800

    initial commit of fundamentals assignment

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i= 1; i <= 255; i++)
+            Console.WriteLine(i);
+            
+            for(int x = 1; x <= 100; x++)
+                if(x % 3 == 0 || x % 5 == 0)
+            Console.WriteLine(x);
+            
+            for(int y = 1; y <= 100; y++)
+            if(y % 3 == 0 && y % 5 == 0)
+            Console.WriteLine("Fizz Buzz");
+            else if(y % 3 == 0)
+            Console.WriteLine("Fizz");
+            else if(y % 5 == 0)
+            Console.WriteLine("Buzz");
+            else
+            Console.WriteLine(y);
+        }
+    }
+}
